0e99d5448e393d2a4688515b1bea11de85226f33
Itay Radotzki <radotzki@gmail.com>
2016-05-20 17:30:28 +0300

    Pretty print

--- src/gene-hmm-train/print.js
+++ src/gene-hmm-train/print.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const _ = require('lodash');
+
 function printStatus(transProb, emisProb, totalProb) {
     console.log(`|  ${transProb['I']['A'].toFixed(2)}  ${transProb['G3']['T'].toFixed(2)}  ${emisProb['I']['A'].toFixed(2)}  ${emisProb['I']['T'].toFixed(2)}  ${emisProb['I']['C'].toFixed(2)}  ${emisProb['G1']['A'].toFixed(2)}  ${emisProb['G1']['T'].toFixed(2)}  ${emisProb['G1']['C'].toFixed(2)}    ${totalProb.toFixed(4)}  |`);
 }
 
 function printHeader(obs, scoreString) {
-    console.log(`|  ${obs}`);
+    console.log('   ' + _.map(obs, s => s + ' ').join(''));
     console.log(`|  -----------------------------------------------------------`);
     console.log(`|  I->G  G->I  I->A  I->T  I->C  G->A  G->T  G->C    score (${scoreString})`);
 }
